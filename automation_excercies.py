46638b7f95e72e7e9d9d451bdd07b12c2c3fe809
sanaullahmeh <sanaullahmehrani4@gmail.com>
2024-12-17 15:44:41 +0500

    Automation Practice Work

--- automation_excercies.py
+++ automation_excercies.py
@@ -13,7 +13,7 @@ class AutomationExcercies:
             EC.presence_of_element_located((By.CSS_SELECTOR,"input[data-qa='signup-email']"))
         )
         email_field.clear()
-        email_field.send_keys("rixxer42@gmail.com")
+        email_field.send_keys("rixxer554@gmail.com")
         time.sleep(1)
         SignUpButton = WebDriverWait(driver , 10).until(
             EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='Signup']"))
@@ -67,12 +67,88 @@ class AutomationExcercies:
         driver.execute_script("arguments[0].scrollIntoView()",Offers)
         Offers.click()
         time.sleep(2)
-
-
-
-
-
-
+    def AddressInformation(self ,driver):
+        first_name = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='first_name']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()",first_name)
+        first_name.clear()
+        first_name.send_keys("Rixxer")
+        time.sleep(2)
+        last_name = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='last_name']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()",first_name)
+        last_name.clear()
+        last_name.send_keys("Deggon")
+        time.sleep(2)
+        company_name = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='company']"))
+        )
+        company_name.clear()
+        company_name.send_keys("Dekkon")
+        time.sleep(2)
+        Address_one = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='address1']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , Address_one)
+        Address_one.clear()
+        Address_one.send_keys("Random Address Number One")
+        time.sleep(1)
+        Address_two = WebDriverWait(driver ,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='address2']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , Address_two)
+        Address_two.clear()
+        Address_two.send_keys("My Random Address Number 2 Street Unknown House No 22")
+        time.sleep(1)
+        country = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//select[@id='country']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , country)
+        country_dropdown = Select(country)
+        country_dropdown.select_by_visible_text("Canada")
+        time.sleep(1)
+        state = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='state']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , state)
+        state.clear()
+        state.send_keys("British Columbia")
+        time.sleep(1)
+        city = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='city']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , city)
+        city.clear()
+        city.send_keys("Vancouver")
+        time.sleep(1)
+        zip_code = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='zipcode']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , zip_code)
+        zip_code.clear()
+        zip_code.send_keys("87689")
+        time.sleep(2)
+        Mobile_number = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//input[@id='mobile_number']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , Mobile_number)
+        Mobile_number.clear()
+        Mobile_number.send_keys("023200892123")
+        time.sleep(1)
+        create_account = WebDriverWait(driver ,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//button[normalize-space()='Create Account']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView()" , Mobile_number)
+        create_account.click()
+        time.sleep(2)
+        continue_button = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.XPATH,"//a[@class='btn btn-primary']"))
+        )
+        continue_button.click()
+        time.sleep(5)
+        print("Automation Excercise Website Signup Page Automated Successfully")
 driver = webdriver.Chrome()
 driver.maximize_window()
 try:
@@ -81,5 +157,6 @@ try:
     AutomationExcerciesObj.RegistrationDetails(driver)
     AutomationExcerciesObj.DateOfBirth(driver)
     AutomationExcerciesObj.NewsLetterSection(driver)
+    AutomationExcerciesObj.AddressInformation(driver)
 finally:
     driver.quit()
