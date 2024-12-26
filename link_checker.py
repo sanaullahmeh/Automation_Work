1e38470b6fa2ff2ce471491dab847e4a41990da1
sanaullahmeh <sanaullahmehrani4@gmail.com>
2024-12-26 13:45:07 +0500

    Automation Practice

---
+++ link_checker.py
@@ -0,0 +1,40 @@
+from selenium_imports.common_imports import *
+
+class LinkChecker:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.base_url = "Enter Your Website URL"
+    
+    def get_links(self):
+        
+        self.driver.get(self.base_url)
+        elements = self.driver.find_elements(By.TAG_NAME, "a")
+        links = [element.get_attribute("href") for element in elements if element.get_attribute("href")]
+        return links
+
+    def check_links(self, links):
+        error_links = []
+        for link in links:
+            try:
+                response = requests.get(link, timeout=10)
+                if 400 <= response.status_code < 600:
+                    error_links.append((link, response.status_code))
+            except requests.exceptions.RequestException as e:
+                error_links.append((link, str(e)))
+        return error_links
+
+    def run(self):
+        links = self.get_links()
+        print(f"Total links found on the website: {len(links)}")
+        error_links = self.check_links(links)
+        print(f"Total No of Error Links - {len(error_links)}")
+        if error_links:
+            print("\nLinks with errors:")
+            for url, status in error_links:
+                print(f"{url} - Error Code/Issue: {status}")
+        else:
+            print("No broken links found!")
+        self.driver.quit()
+checker = LinkChecker()
+checker.run()
--- selenium_imports/common_imports.py
+++ selenium_imports/common_imports.py
@@ -7,10 +7,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+#from datetime import datetime , delta
 from selenium.webdriver.support.ui import Select
-
+import pytest
+from selenium.webdriver.common.print_page_options import PrintOptions
 from selenium.webdriver.chrome.options import Options
 import time
 import os
 import sys
 import logging
+import requests
